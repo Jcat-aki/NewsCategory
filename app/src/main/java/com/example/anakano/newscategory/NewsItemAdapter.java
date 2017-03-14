d2072e150b05490158fb43839859ea3ca5e2473e
J-aki <nakano1akihiro@gmail.com>
2017-03-15 03:39:23 +0900

    listviewに一旦表示しました。

--- app/src/main/java/com/example/anakano/newscategory/NewsItemAdapter.java
+++ app/src/main/java/com/example/anakano/newscategory/NewsItemAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +27,7 @@ public class NewsItemAdapter extends ArrayAdapter<NewsItem> {
     public NewsItemAdapter(@NonNull Context context, @LayoutRes int resource) {
         super(context, resource);
         mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mNewsItems = new ArrayList<>();
     }
 
     @Override
@@ -35,7 +40,30 @@ public class NewsItemAdapter extends ArrayAdapter<NewsItem> {
         return mNewsItems.get(position);
     }
 
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent){
+        View view = convertView;
+        ViewHolder viewHolder;
+
+        if( view == null ){
+            view = mInflater.inflate(R.layout.news_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.titleView = (TextView)view.findViewById(R.id.title_item);
+            view.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.titleView.setText(mNewsItems.get(position).title);
+
+        return view;
+    }
+
     public void setData(List<NewsItem> newsItems){
         mNewsItems = newsItems;
+    }
+
+    private static class ViewHolder {
+        TextView titleView;
     }
 }
